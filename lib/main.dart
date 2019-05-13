41c1a5fb4dcb22c9f11f526bd17a17117f571ef7
Ashrf Ali <asifshatvm@gmail.com>
2019-05-14 00:16:15 +0400

    completed the business card app with static image and text contents

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'SourceSansPro'),
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 75.0,
+                backgroundImage: NetworkImage(
+                    'https://avatars1.githubusercontent.com/u/16478713?s=400&u=15caeba7e048c5a5c96966dae2287210ec77ad55&v=4'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
+              Text(
+                'Asharaf Ali',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              Text(
+                'Full Stack Dev'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 3.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                ),
+              ),
+              SizedBox(
+                width: 250.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal[200],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+971 52 7490 744',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'ashrftvm@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
